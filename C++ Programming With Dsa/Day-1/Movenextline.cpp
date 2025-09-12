0b2aee7e18e34b6cec582fce430d2ac14fb2eed6
VIRENDRA-18 <virusingh0125@gmail.com>
2025-09-12 22:10:10 +0530

    Refactor code structure for improved readability and maintainability

---
+++ C++ Programming With Dsa/Day-1/Movenextline.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    // How to move next line
+    cout<<"Hi Virendra"<<endl<<"Hey! Kumkum";
+    // cout<<endl;
+    // cout<<"Hey! Kumkum";
+}
---
+++ C++ Programming With Dsa/Day-1/Printingnumber.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // Printing Number 
+    cout<<4<<endl;
+    cout<<5+7<<endl;
+    cout<<"8+9"; // count as a string because of double quotes
+    return 0;
+}
