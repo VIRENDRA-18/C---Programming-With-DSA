708e2c091a25ca6e6bd189f8f6dc527109450e3b
VIRENDRA-18 <virusingh0125@gmail.com>
2025-09-30 21:45:25 +0530

    learn about for loops

---
+++ C++ Programming With Dsa/Loops-1/Greeting.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number : "; // Prompt the user to enter a number
+    cin>>n;                  // Promt Take a User Input
+
+    // Print "Hello" n times using a loop
+    for(int i=1; i<=n; i++){
+        cout<<"Hello Virendra!"<<endl;
+    }
+}
